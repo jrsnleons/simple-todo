5a337f1f357ac1b223b80209f3d78a62c5d4423f
jj_leo <jrsnleons@gmail.com>
2023-04-16 23:26:43 +0800

    Add project files.

---
+++ Todo/Todo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Todo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            //initialize some sample data
+            todo.ItemsSource = new string[] { "Do a task", "Add a task", "Create a new application", "Play Pokemon", "Pass Mobile Daevelopment Course" };
+        }
+        
+        private void addATodo(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new addTodo());
+        }
+    }
+}
